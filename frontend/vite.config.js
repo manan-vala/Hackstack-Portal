bfff7fb46fe64e835e6f7d28d590db22bc710630
Manan Vala <mananvala15@gmail.com>
2026-06-07 20:03:36 +0000

    vite config changed

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/hackstack/",
+  base: "/",
   plugins: [react(), tailwindcss()],
   build: {
     rollupOptions: {
